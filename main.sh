57eb3ce09d9533223701d4a64b3f7db5a357f36f
bndw <benjamindwoodward@gmail.com>
2020-04-26 21:54:28 -0700

    :lipstick: Formatting

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Adapted from https://willwarren.com/2014/07/03/roll-dynamic-dns-service-using-amazon-route53/
+set -e
 
 # Hosted Zone ID e.g. BJBK35SKMM9OE
 : ${ZONEID?"Must set ZONEID, e.g. BJBK35SKMM9OE"}
@@ -7,69 +8,66 @@
 # The CNAME you want to update e.g. hello.example.com
 : ${CNAME?"Must set CNAME, e.g. hello.example.com"}
 
-# The Time-To-Live of this CNAME
+# The Time-To-Live of the CNAME
 TTL="${TTL:-300}"
 
-# Change this if you want
-COMMENT="Updated by bndw/dyndns @ `date`"
 # Change to AAAA if using an IPv6 address
 TYPE="A"
 
-# Get the external IP address from OpenDNS (more reliable than other providers)
+# Get the external IP address from OpenDNS
 IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 
-function valid_ip()
-{
-    local  ip=$1
-    local  stat=1
+valid_ip() {
+  local ip=$1
+  local stat=1
 
-    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-        OIFS=$IFS
-        IFS='.'
-        ip=($ip)
-        IFS=$OIFS
-        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
-            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
-        stat=$?
-    fi
-    return $stat
+  if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+    OIFS=$IFS
+    IFS='.'
+    ip=($ip)
+    IFS=$OIFS
+    [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+      && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+    stat=$?
+  fi
+  return $stat
 }
 
-if ! valid_ip $IP; then
-    echo "Invalid IP address: $IP"
-    exit 1
+if ! valid_ip "${IP}" ; then
+  echo "Invalid IP address: ${IP}"
+  exit 1
 fi
 
 TMPFILE=$(mktemp /tmp/temporary-file.XXXXXXXX)
-cat > ${TMPFILE} << EOF
+cat > "${TMPFILE}" << EOF
+{
+  "Comment":"Updated by bndw/dyndns @ `date`",
+  "Changes":[
     {
-      "Comment":"$COMMENT",
-      "Changes":[
-        {
-          "Action":"UPSERT",
-          "ResourceRecordSet":{
-            "ResourceRecords":[
-              {
-                "Value":"$IP"
-              }
-            ],
-            "Name":"$CNAME",
-            "Type":"$TYPE",
-            "TTL":$TTL
+      "Action":"UPSERT",
+      "ResourceRecordSet":{
+        "ResourceRecords":[
+          {
+            "Value":"${IP}"
           }
-        }
-      ]
+        ],
+        "Name":"${CNAME}",
+        "Type":"${TYPE}",
+        "TTL":${TTL}
+      }
     }
+  ]
+}
 EOF
 
 printf "Setting ${CNAME} → ${IP}\n"
 
 # Update the Hosted Zone record
 aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONEID \
-    --change-batch file://"$TMPFILE" \
-    --output json \
-    | jq '.ChangeInfo.Id' \
-    | xargs -I{} aws route53 wait resource-record-sets-changed --id {}
+  --hosted-zone-id "${ZONEID}" \
+  --change-batch "file://${TMPFILE}" \
+  --output json \
+  | jq '.ChangeInfo.Id' \
+  | xargs -I{} aws route53 wait resource-record-sets-changed --id {}
 
-rm $TMPFILE
+rm "${TMPFILE}"
